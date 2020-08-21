05c3a49c238edce5694529d00d7211fb092e25f4
evenvagen <evenvagen@gmail.com>
2020-08-21 14:25:57 +0200

    Initial commit

---
+++ DomainModel.Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Core
+{
+    class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public BaseEntity(Guid id)
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
