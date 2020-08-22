88f0bdff9d9a8c09d81ce896dcf8c09db61c84df
evenvagen <evenvagen@gmail.com>
2020-08-22 16:45:13 +0200

    twotests

--- LayerTests/ApplicationServiceMoq.cs
+++ LayerTests/ApplicationServiceMoq.cs
@@ -29,8 +29,40 @@ namespace LayerTests
             var subscribeIsSuccess = await service.Subscribe(newSubscriber);
 
             //Act
-
             Assert.IsTrue(subscribeIsSuccess);
+
+
+            emailInterfaceMock.Verify(es => es.Send(It.Is<Email>(n => n.To == "evenv@mail.net")), Times.Exactly(1));
+
+            subscriptionInterfaceMock.Verify(ns => ns.Create(It.Is<Subscription>(n => n.Name == "Even")), Times.Once);
+
+            //AllCalls
+            emailInterfaceMock.VerifyNoOtherCalls();
+            subscriptionInterfaceMock.VerifyNoOtherCalls();
+        }
+
+
+        [Test]
+        public async Task checkEmailServiceFail()
+        {
+            var emailInterfaceMock = new Mock<IEmailService>();
+            var subscriptionInterfaceMock = new Mock<ISubsciptionRepository>();
+
+            subscriptionInterfaceMock.Setup(sub => sub.Create(It.IsAny<Subscription>())).ReturnsAsync(false);
+
+
+            //Arrange 
+            var subscriptionService = new SubscriptionService(emailInterfaceMock.Object, subscriptionInterfaceMock.Object);
+            var subscription = new Subscription("Even", "evenvagen@mail.net");
+            var go = await subscriptionService.Subscribe(subscription);
+
+
+            Assert.IsFalse(go);
+
+            subscriptionInterfaceMock.Verify(sub => sub.Create(It.Is<Subscription>(m => m.Email == "evenvagen@mail.net")));
+
+            emailInterfaceMock.VerifyNoOtherCalls();
+            subscriptionInterfaceMock.VerifyNoOtherCalls();
         }
     }
 }
