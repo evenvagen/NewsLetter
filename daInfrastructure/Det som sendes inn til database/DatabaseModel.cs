8464ce48a09d23452357785ae3c83f7e527faedb
evenvagen <evenvagen@gmail.com>
2020-08-23 16:25:42 +0200

    x
    Lagt til hva som sendes til SQL serveren, må fikse connection-string

---
+++ daInfrastructure/Det som sendes inn til database/DatabaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace daInfrastructure
+{
+    class DatabaseModel
+    {
+        public string Email { get; set; }
+        public Guid ConfirmationGuid { get; set; }
+
+        public DatabaseModel(string email, Guid confirmationGuid)
+        {
+            Email = email;
+            ConfirmationGuid = confirmationGuid;
+        }
+    }
+}
