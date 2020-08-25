88840a941a23906990d5a6ee58a870671f6ebfbe
evenvagen <evenvagen@gmail.com>
2020-08-25 15:05:22 +0200

    nukk

--- DomainModel.Core/Subscription.cs
+++ DomainModel.Core/Subscription.cs
@@ -17,7 +17,7 @@ namespace DomainModel.Core
         public Subscription(string name, string email, string verificationCode = null)
         {
             Name = name;
-            Email = email ?? "null@mail.net";
+            Email = email;
             VerificationCode = verificationCode ?? Guid.NewGuid().ToString();
         }
 
--- NewsLetter/ViewModel/Subscription.cs
+++ NewsLetter/ViewModel/Subscription.cs
@@ -12,17 +12,17 @@ namespace NewsLetter.ViewModel
         public Guid Id { get; set; }
         public bool IsVerified { get; set; }
 
-        public Subscription(string name, string email, Guid id, bool isVerified)
-        {
-            Name = name;
-            Email = email;
-            Id = id;
-            IsVerified = isVerified;
-        }
+        //public Subscription(string name, string email, Guid id, bool isVerified)
+        //{
+        //    Name = name;
+        //    Email = email;
+        //    Id = id;
+        //    IsVerified = isVerified;
+        //}
 
-        public Subscription()
-        {
-            
-        }
+        //public Subscription()
+        //{
+
+        //}
     }
 }
